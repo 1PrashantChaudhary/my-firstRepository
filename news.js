2a86b965e6301fa967d60cba230372908de8a3b9
Prashant chaudhary <prashantchahal123@gmail.com>
2022-11-14 19:50:46 +0530

    some changes in CSS

---
+++ news.js
@@ -0,0 +1,26 @@
+// document.body.style.backgroundColor="yellow";
+// document.body.style.backgroundColor="Red";
+let a=document.getElementsByClassName("mouseover")[0];
+let b=document.getElementsByClassName("mouseover")[1];
+let c=document.getElementsByClassName("mouseover")[2];
+let d=document.getElementsByClassName("mouseover")[3];
+let e=document.getElementsByClassName("mouseover")[4];
+let f=document.getElementsByClassName("mouseover")[5];
+let g=document.getElementsByClassName("line")[0];
+let h=document.getElementsByClassName("line2")[0];
+let i=document.getElementsByClassName("line3")[0];
+let j=document.getElementsByClassName("line4")[0];
+let k=document.getElementsByClassName("line5")[0];
+let l=document.getElementsByClassName("line6")[0];
+a.onmouseover=function(){g.style.display="block";}
+a.onmouseout=function(){g.style.display="none";}
+b.onmouseover=function(){h.style.display="block";}
+b.onmouseout=function(){h.style.display="none";}
+c.onmouseover=function(){i.style.display="block";}
+c.onmouseout=function(){i.style.display="none";}
+d.onmouseover=function(){j.style.display="block";}
+d.onmouseout=function(){j.style.display="none";}
+e.onmouseover=function(){k.style.display="block";}
+e.onmouseout=function(){k.style.display="none";}
+f.onmouseover=function(){l.style.display="block";}
+f.onmouseout=function(){l.style.display="none";}
